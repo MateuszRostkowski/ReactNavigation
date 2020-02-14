d9bb0b596ac93284fa1989bc117971891dde25fe
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-02-14 15:34:13 +0100

    add Icons to tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import 'react-native-gesture-handler'
-
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons'
+import FeatherIcon from 'react-native-vector-icons/Feather'
 
-function HomeScreen({ navigation }){
+function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screens</Text>
       <Button
         title="Go to Details"
@@ -18,11 +19,11 @@ function HomeScreen({ navigation }){
   );
 }
 
-function DetailsScreen({ navigation }) {
+function DetailsScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
-      <Button 
+      <Button
         title="Go to Details... again(it makes nothing on app)"
         onPress={() => navigation.navigate('Details')}
       />
@@ -30,14 +31,8 @@ function DetailsScreen({ navigation }) {
         title="Go to Details... again(it adds new cards to stack)"
         onPress={() => navigation.push('Details')}
       />
-      <Button 
-        title="Go home"
-        onPress={() => navigation.navigate('Home')}
-      />
-      <Button 
-        title="Go back"
-        onPress={() => navigation.goBack()}
-      />
+      <Button title="Go home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Go back to first screen in stack"
         onPress={() => navigation.popToTop()}
@@ -52,8 +47,24 @@ function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={DetailsScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({color, size}) => {
+              return <FeatherIcon name="home" color={color} size={size} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={DetailsScreen}
+          options={{
+            tabBarIcon: ({color, size}) => {
+              return <FeatherIcon name="settings" color={color} size={size} />;
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
